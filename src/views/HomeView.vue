a864b02076a7dc5e485ab30e8ffedbe78331a63c
Alex Plate <AlexPl292@gmail.com>
2023-06-17 14:34:16 +0300

    Switch sections in russian language

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -161,7 +161,15 @@ export default {
           <div class="vr" style="height: 10em"></div>
 
           <p style="font-size: 5rem">Deep dive into Russian language</p>
-          <p style="font-size: 3rem">Section 1: Writing</p>
+          <p style="font-size: 3rem">Section 1: Reading</p>
+          <p style="font-size: 15rem">Плáтэ</p>
+          <h3>Do not rush here</h3>
+          <h3>
+            Take a slow and clear look at the word and try to understand where to put an accent
+          </h3>
+          <div class="vr" style="height: 10em"></div>
+
+          <p style="font-size: 3rem">Section 2: Writing</p>
           <p style="font-size: 15rem">Плат<span class="text-danger">э</span></p>
           <h3>The right writing looks like this</h3>
           <h3>
@@ -170,13 +178,6 @@ export default {
           </h3>
           <div class="vr" style="height: 5em"></div>
 
-          <p style="font-size: 3rem">Section 2: Reading</p>
-          <p style="font-size: 15rem">Плáтэ</p>
-          <h3>Do not rush here</h3>
-          <h3>
-            Take a slow and clear look at the word and try to understand where to put an accent
-          </h3>
-          <div class="vr" style="height: 10em"></div>
 
           <p class="fs-1">
             The natural feeling of beauty should guide you when you write or say this surename in
