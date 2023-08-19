a9cec55c5b58e5301635d682d938b602a789a865
Alex Plate <AlexPl292@gmail.com>
2023-08-19 10:40:24 +0300

    Just disable web page in mobile

---
+++ src/views/MobileNotSupported.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Sorry, mobile is not supported :(</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'mobile-not-supported'
+}
+</script>
---
+++ src/views/MobileOrBrowser.vue
@@ -0,0 +1,21 @@
+<template>
+  <mobile-not-supported v-if="isMobile()" />
+  <HomeView v-else />
+</template>
+
+<script lang="ts">
+import HomeView from '@/views/HomeView.vue'
+import MobileNotSupported from '@/views/MobileNotSupported.vue'
+
+export default {
+  name: 'MobileOrBrowser',
+  components: { MobileNotSupported, HomeView },
+  methods: {
+    isMobile() {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    }
+  }
+}
+</script>
